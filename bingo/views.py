6793e96c4988046cd48fe10536dc04896e869f4d
khw0512 <hwunderstand@gmail.com>
2025-04-03 00:16:39 +0900

    20250403-1

--- bingo/views.py
+++ bingo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from django.db.models import Sum
 from django.core.paginator import Paginator
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 from bingo.models import *
 from record.models import Record
@@ -29,3 +32,26 @@ def create_bingo(request, pk):
             bingocheck.save()
         i+=1
     return redirect("bingo:bingo", pk)
+
+
+def update_public_status(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        print(data)
+        bingo_id = data.get("bingo_id")
+        is_checked = data.get("is_checked")
+        print(is_checked)
+
+        try:
+            bingo_item = BingoCheck.objects.get(bingo_item__item_id=bingo_id)
+            if is_checked == False:
+                bingo_item.completed = False
+                bingo_item.save()
+            else:
+                bingo_item.completed = True
+                bingo_item.save()
+            return JsonResponse({"status": "success", "checked": bingo_item.completed})
+        except BingoCheck.DoesNotExist:
+            return JsonResponse({"status": "error", "message": "Post not found"}, status=404)
+
+    return JsonResponse({"error": "Invalid request"}, status=400)
