555cf7c070b9985cf0f44485b13de8ab732a0828
khw0512 <hwunderstand@gmail.com>
2025-04-03 13:58:24 +0900

    20250403-6

--- bingo/views.py
+++ bingo/views.py
@@ -9,18 +9,18 @@ from bingo.models import *
 from record.models import Record
 
 def bingo(request, pk):
-    bingo_item_1r = BingoCheck.objects.select_related("bingo_item").filter(bingo_item__row=1)
-    bingo_item_2r = BingoCheck.objects.select_related("bingo_item").filter(bingo_item__row=2)
-    bingo_item_3r = BingoCheck.objects.select_related("bingo_item").filter(bingo_item__row=3)
-    bingo_item_4r = BingoCheck.objects.select_related("bingo_item").filter(bingo_item__row=4)
-    bingo_item_5r = BingoCheck.objects.select_related("bingo_item").filter(bingo_item__row=5)
+    bingo_item_1r = BingoCheck.objects.select_related("bingo_item").filter(user=pk).filter(bingo_item__row=1)
+    bingo_item_2r = BingoCheck.objects.select_related("bingo_item").filter(user=pk).filter(bingo_item__row=2)
+    bingo_item_3r = BingoCheck.objects.select_related("bingo_item").filter(user=pk).filter(bingo_item__row=3)
+    bingo_item_4r = BingoCheck.objects.select_related("bingo_item").filter(user=pk).filter(bingo_item__row=4)
+    bingo_item_5r = BingoCheck.objects.select_related("bingo_item").filter(user=pk).filter(bingo_item__row=5)
     print(bingo_item_1r)
     return render(request,"bingo.html",{"bingo_item_1r":bingo_item_1r, "bingo_item_2r":bingo_item_2r, "bingo_item_3r":bingo_item_3r, "bingo_item_4r":bingo_item_4r, "bingo_item_5r":bingo_item_5r})
 
 def create_bingo(request, pk):
     i = 1
     while i <= 25:
-        bingoitem = BingoItem.objects.filter(item_id = i    )
+        bingoitem = BingoItem.objects.filter(item_id = i)
         existing = BingoCheck.objects.filter(bingo_item = bingoitem[0]).exists()
         print(bingoitem)
         if not existing:
