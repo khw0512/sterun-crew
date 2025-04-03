2fff715d25b391ccac2943369f950c1405a0a3eb
khw0512 <hwunderstand@gmail.com>
2025-04-03 22:11:35 +0900

    20250403-7

--- bingo/urls.py
+++ bingo/urls.py
@@ -7,5 +7,5 @@ app_name = "bingo"
 urlpatterns = [
     path("<str:pk>", views.bingo, name="bingo"),
     path("createbingo/<str:pk>", views.create_bingo, name="create_bingo"),
-    path("update-public-status/", views.update_public_status, name="update_public_status"),
+    path("update-public-status/<str:pk>", views.update_public_status, name="update_public_status"),
 ]
--- bingo/views.py
+++ bingo/views.py
@@ -15,13 +15,13 @@ def bingo(request, pk):
     bingo_item_4r = BingoCheck.objects.select_related("bingo_item").filter(user=pk).filter(bingo_item__row=4)
     bingo_item_5r = BingoCheck.objects.select_related("bingo_item").filter(user=pk).filter(bingo_item__row=5)
     print(bingo_item_1r)
-    return render(request,"bingo.html",{"bingo_item_1r":bingo_item_1r, "bingo_item_2r":bingo_item_2r, "bingo_item_3r":bingo_item_3r, "bingo_item_4r":bingo_item_4r, "bingo_item_5r":bingo_item_5r})
+    return render(request,"bingo.html",{"bingo_item_1r":bingo_item_1r, "bingo_item_2r":bingo_item_2r, "bingo_item_3r":bingo_item_3r, "bingo_item_4r":bingo_item_4r, "bingo_item_5r":bingo_item_5r, "user_id":pk})
 
 def create_bingo(request, pk):
     i = 1
     while i <= 25:
         bingoitem = BingoItem.objects.filter(item_id = i)
-        existing = BingoCheck.objects.filter(bingo_item = bingoitem[0]).exists()
+        existing = BingoCheck.objects.filter(bingo_item = bingoitem[0]).filter(user=request.user).exists()
         print(bingoitem)
         if not existing:
             bingocheck = BingoCheck(
@@ -30,11 +30,12 @@ def create_bingo(request, pk):
                 completed = False
             )
             bingocheck.save()
+            print(request.user)
         i+=1
     return redirect("bingo:bingo", pk)
 
 
-def update_public_status(request):
+def update_public_status(request,pk):
     if request.method == "POST":
         data = json.loads(request.body)
         print(data)
@@ -43,7 +44,7 @@ def update_public_status(request):
         print(is_checked)
 
         try:
-            bingo_item = BingoCheck.objects.get(bingo_item__item_id=bingo_id)
+            bingo_item = BingoCheck.objects.filter(user=request.user).get(bingo_item__item_id=bingo_id)
             if is_checked == False:
                 bingo_item.completed = False
                 bingo_item.save()
