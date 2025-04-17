5ca80983f59675a934c0d7a3f5f6ea60346f3881
khw0512 <hwunderstand@gmail.com>
2025-04-18 00:06:10 +0900

    250418-007

--- record/urls.py
+++ record/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("resister/", views.runres, name="runres"),
     path('recordres/', views.recordres, name='recordres'),
     path('recordview/<str:pk>/', views.recordview, name='recordview'),
+    path('recordUpdateView/<str:pk>/<str:record_id>', views.recordUpdateView, name='recordUpdateView'),
+    path('recordUpdate/<str:pk>/<str:record_id>', views.recordUpdate, name='recordUpdate'),
 ]
--- record/views.py
+++ record/views.py
@@ -48,3 +48,26 @@ def recordview(request,pk):
     record_list = paginator.get_page(page)
 
     return render(request,"run_record.html", {"context":record_list, "distance_sum":distance_sum, "month": month, "distance_sum_not":distance_sum_not})
+
+def recordUpdateView(request,pk,record_id):
+    record = Record.objects.filter(user=pk).get(record_id=record_id)
+    return render(request,"run_update.html", {"record_id":record_id, "record":record})
+
+def recordUpdate(request,pk,record_id):
+    record = Record.objects.filter(user=pk).get(record_id=record_id)
+    if request.method == "POST":
+        record.title = request.POST["title"]
+        record.user = request.user
+        record.record_date = request.POST["record_date"]
+        if request.FILES.get("image") is not None:
+            record.image = request.FILES["image"]
+        record.distance = request.POST["distance"]
+        record.pace_m = request.POST["pace_m"]
+        record.pace_s = request.POST["pace_s"]
+        record.time_m = request.POST["time_m"]
+        record.time_s = request.POST["time_s"]
+        record.desc = request.POST["desc"]
+        record.save()
+        return render(request, "index.html")
+    else:
+        return render(request, "index.html")
