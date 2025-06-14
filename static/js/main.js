b60b55fca26a08c30e2570dea730d6d1c58ac705
khw0512 <hwunderstand@gmail.com>
2025-06-15 01:00:01 +0900

    20250625002

--- static/js/main.js
+++ static/js/main.js
@@ -37,12 +37,27 @@ window.onload = function(){
 const mask = document.querySelector('.mask');
 const html = document.querySelector('html');
 
-html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
+html.style.overflow = 'auto'; //로딩 중 스크롤 방지
 window.addEventListener('load', function () {
   //아래 setTimeout은 로딩되는 과정을 임의로 생성하기 위해 사용. 실제 적용 시에는 삭제 후 적용해야함.
   setTimeout(function () {
-    mask.style.opacity = '0'; //서서히 사라지는 효과
     html.style.overflow = 'auto'; //스크롤 방지 해제
     mask.style.display = 'none';
-  }, 500);
+  }, 1000);
 })
+
+//preloader
+var preloader = function () {
+  setTimeout(function () {
+    $(".preload-container").fadeOut("slow", function () {
+      $(this).remove();
+    });
+  }, 1000);
+}; //preloader
+
+$(function () {
+  if (matchMedia("only screen and (min-width: 991px)").matches) {
+    headerFixed();
+  }
+  preloader();
+});
