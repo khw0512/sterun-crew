cf76d9ab4832c401dd71e56be48a85467d41b43e
khw0512 <hwunderstand@gmail.com>
2025-06-15 01:20:14 +0900

    20250625007

--- static/js/main.js
+++ static/js/main.js
@@ -38,7 +38,7 @@ const mask = document.querySelector('.mask');
 const html = document.querySelector('html');
 
 html.style.overflow = 'auto'; //로딩 중 스크롤 방지
-window.addEventListener('load', function () {
+window.addEventListener('DOMContentLoaded', function () {
   //아래 setTimeout은 로딩되는 과정을 임의로 생성하기 위해 사용. 실제 적용 시에는 삭제 후 적용해야함.
   setTimeout(function () {
     html.style.overflow = 'auto'; //스크롤 방지 해제
