e3f7ea10f3bf8442a16bbf7c9ec8bb3506eae83c
khw0512 <hwunderstand@gmail.com>
2025-11-13 14:14:35 +0900

    20251113-002

--- marathon/models.py
+++ marathon/models.py
@@ -7,6 +7,8 @@ class MarathonEvent(models.Model):
     marathon_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, blank=False, null=False)
     date_time = models.DateTimeField()
+    location_city = models.CharField(max_length=50)
+    location_cource = models.CharField(max_length=50)
     url = models.URLField()
 
     def __str__(self):
@@ -15,6 +17,7 @@ class MarathonEvent(models.Model):
 
 class MarathonReg(models.Model):
     marathon = models.ForeignKey(MarathonEvent, on_delete=models.CASCADE)
+    distance = models.CharField(max_length=10, blank=False, null=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
